807236ac33fd032fc673ec16a1b5c9c8596a6171
trevorr-droid <82988255+trevorr-droid@users.noreply.github.com>
2026-05-02 22:39:36 -0600

    Update main.tf

---
+++ modules/ecr-repo/main.tf
@@ -0,0 +1,38 @@
+resource "aws_ecr_repository" "this" {
+  name = var.name
+
+  image_tag_mutability = var.image_tag_mutability
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+
+  encryption_configuration {
+    encryption_type = var.encryption_type
+    kms_key         = var.kms_key
+  }
+
+  tags = var.tags
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  count      = var.enable_lifecycle_policy ? 1 : 0
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images older than 30 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 30
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
