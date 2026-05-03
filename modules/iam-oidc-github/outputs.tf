cab717c2c7819817dbef2af4e8699c3e4a5b8a4f
trevorr-droid <82988255+trevorr-droid@users.noreply.github.com>
2026-05-02 22:15:57 -0600

    Update outputs.tf

---
+++ modules/iam-oidc-github/outputs.tf
@@ -0,0 +1,9 @@
+output "role_arn" {
+  description = "ARN of the IAM role GitHub Actions can assume"
+  value       = aws_iam_role.github_actions.arn
+}
+
+output "oidc_provider_arn" {
+  description = "ARN of the GitHub OIDC provider"
+  value       = aws_iam_openid_connect_provider.github.arn
+}
