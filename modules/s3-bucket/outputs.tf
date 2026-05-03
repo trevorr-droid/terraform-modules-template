ccffdd78ed8f00dcc6572d1484f82d825c5de9d7
trevorr-droid <82988255+trevorr-droid@users.noreply.github.com>
2026-05-02 21:57:00 -0600

    Add files via upload

---
+++ modules/s3-bucket/outputs.tf
@@ -0,0 +1,10 @@
+output "bucket_name" {
+  description = "The name of the bucket"
+  value       = aws_s3_bucket.this.bucket
+}
+
+output "bucket_arn" {
+  description = "The ARN of the bucket"
+  value       = aws_s3_bucket.this.arn
+}
+
