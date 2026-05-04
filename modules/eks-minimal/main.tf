650f15afd7b5a5db712293b355e244f513fbda3e
trevorr-droid <82988255+trevorr-droid@users.noreply.github.com>
2026-05-04 08:32:23 -0600

    Add EKS cluster and node group resources

---
+++ modules/eks-minimal/main.tf
@@ -0,0 +1,78 @@
+resource "aws_iam_role" "cluster" {
+  name               = "${var.name}-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.cluster_assume_role.json
+  tags               = var.tags
+}
+
+data "aws_iam_policy_document" "cluster_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = var.name
+  role_arn = aws_iam_role.cluster.arn
+  version  = var.cluster_version
+
+  vpc_config {
+    subnet_ids = concat(var.private_subnet_ids, var.public_subnet_ids)
+  }
+
+  tags = var.tags
+}
+
+resource "aws_iam_role" "node_group" {
+  for_each = var.node_groups
+
+  name               = "${var.name}-${each.key}-ng-role"
+  assume_role_policy = data.aws_iam_policy_document.node_group_assume_role.json
+  tags               = var.tags
+}
+
+data "aws_iam_policy_document" "node_group_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_eks_node_group" "this" {
+  for_each = var.node_groups
+
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = each.key
+  node_role_arn   = aws_iam_role.node_group[each.key].arn
+  subnet_ids      = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = each.value.desired_size
+    min_size     = each.value.min_size
+    max_size     = each.value.max_size
+  }
+
+  instance_types = each.value.instance_types
+  disk_size      = each.value.disk_size
+
+  labels = each.value.labels
+
+  dynamic "taint" {
+    for_each = each.value.taints
+    content {
+      key    = taint.value.key
+      value  = taint.value.value
+      effect = taint.value.effect
+    }
+  }
+
+  tags = var.tags
+}
